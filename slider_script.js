6a2367b1525518aa8c60ce244f89a9a332fbcfcd
volosovrseva <nast6ka@list.ru>
2017-01-25 13:12:58 +0200

    pagination

--- slider_script.js
+++ slider_script.js
@@ -6,6 +6,7 @@ var slider;
 var timer;
 var prevButton;
 var nextButton;
+var pages;
 
 function init() {
 
@@ -19,33 +20,51 @@ function init() {
     sliderBox.style.height = parseInt(slidesLi[0].children[0].clientHeight) + "px";
     sliderBox.style.width = imageWidth + "px";
 
-    timer = setInterval(SliderFunction, 3000);
+    pages = document.getElementById("pages");
+    for (i = 0; i < imageNumber; i++) {
+        var liPage = document.createElement("li");
+        liPage.className = "page";
+        liPage.id = i;
+        pages.appendChild(liPage);
+    }
 
     prevButton = document.getElementById("prev");
     nextButton = document.getElementById("next");
+
+    timer = setInterval(SliderFunction, 3000);
+
+    pages.onclick = function () { goToImage(event); };
 
     prevButton.onclick = function () { prevButtonClick(); };
     nextButton.onclick = function () { nextButtonClick(); };
 
     sliderBox.onmouseover = mouseOver;
     sliderBox.onmouseout = mouseOut;
+}
 
 
+function goToImage(event){
+    var page = event.target;
+    currentImage = page.id;
+    left = currentImage * imageWidth * (-1);
+    slider.style.left = left + "px";
 }
 function mouseOut(){
-    prevButton.style.visibility = "";
-    nextButton.style.visibility = "";
+    pages.style.display = "";
+    prevButton.style.display = "";
+    nextButton.style.display = "";
     timer = setInterval(SliderFunction, 3000);
 }
 function mouseOver(){
-    prevButton.style.visibility = "visible";
-    nextButton.style.visibility = "visible";
+    pages.style.display = "block";
+    prevButton.style.display = "block";
+    nextButton.style.display = "block";
     clearInterval(timer);
 }
 function nextButtonClick(){
     if (currentImage < imageNumber-1){
         currentImage++;
-        left = (currentImage) * imageWidth * (-1);
+        left = currentImage * imageWidth * (-1);
         slider.style.left = left + "px";
 
     }else if(currentImage == imageNumber-1) {
